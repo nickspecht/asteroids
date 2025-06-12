eb54182ddce4c84d438c30a10bc8349e4976d938
awesomo <nickspecht777@gmail.com>
2025-06-11 20:11:10 -0500

    Added black screen, ability to close window, and FPS

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+clock = pygame.time.Clock()
+dt = 0
 def main():
     print("Starting Asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
@@ -12,5 +14,6 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 if __name__ == "__main__":
     main()
