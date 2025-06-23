e2196539895952e20de102b7c7a7dd1cde1c5f5a
awesomo <nickspecht777@gmail.com>
2025-06-22 19:43:10 -0500

    Added asteroids to the screen that move in straight line

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import pygame
+import pygame 
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 clock = pygame.time.Clock()
 dt = 0
 def main():
@@ -10,7 +12,11 @@ def main():
     pygame.init()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
